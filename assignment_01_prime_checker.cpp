a4b74f4955664a124e99f2e9fde358c40f6a1a7e
AAKA-104 <akaappiah002@st.ug.edu.gh>
2026-08-01 22:05:11 +0000

    Assignment 1 cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,47 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if a number is prime
+bool isPrime(int num) {
+    // Numbers less than 2 are not prime
+    if (num < 2) {
+        return false;
+    }
+    
+    // 2 is the smallest prime number
+    if (num == 2) {
+        return true;
+    }
+    
+    // Even numbers greater than 2 are not prime
+    if (num % 2 == 0) {
+        return false;
+    }
+    
+    // Check odd divisors up to sqrt(num)
+    // Using i*i <= num avoids needing sqrt() from cmath
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            return false;  // Found a divisor, not prime
+        }
+    }
+    
+    return true;  // No divisors found, number is prime
+}
+
+int main() {
+    int number;
+    
+    // Get input from user
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    // Check if number is prime and display result
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+    
+    return 0;
+}
